c538dafb76037c94b4e32659cbc44ba655e48c5c
Harun <harunalrashyid@gmail.com>
2020-05-17 08:04:07 +0700

    update - navbar toggle mobile using reactHooks

--- src/shared/layouts/Header.js
+++ src/shared/layouts/Header.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Header = props => {
+    // sorry not using redux instead reactHooks to update simple state
+    const [navToggle, setNavToggle] = useState(false)
+
     return (
         <header className="Header">
             <div className="container">
@@ -13,7 +16,7 @@ const Header = props => {
                         </div>
                     </div>
                     <div className="Header__right col-md-8">
-                        <div className="Header__nav">
+                        <div className={ navToggle ? 'Header__nav Header__nav--toggle' : 'Header__nav' }>
                             <ul className="Header__nav-list">
                                 <li className="Header__nav-item">
                                     <a href="#" className="Header__nav-link">Home</a>
@@ -24,13 +27,13 @@ const Header = props => {
                                 <li className="Header__nav-item">
                                     <a href="#" className="Header__nav-link">Apply Here</a>
                                 </li>
-                                <li className="Header__item">
+                                <li className="Header__nav-item">
                                     <a href="#" className="Header__nav-link Header__nav-link--last">FAQ</a>
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    <div className="Header__nav-toggle" onClick={() => console.log('clicked')}>
+                    <div className="Header__nav-toggle" onClick={() => setNavToggle(!navToggle)}>
                         <span className="Header__nav-toggle-stripe Header__nav-toggle-stripe--1"></span>
                         <span className="Header__nav-toggle-stripe Header__nav-toggle-stripe--2"></span>
                         <span className="Header__nav-toggle-stripe Header__nav-toggle-stripe--3"></span>
